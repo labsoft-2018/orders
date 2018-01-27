1a0a7eb12cf4390aad816e22bdc472f3662dcefb
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-27 15:15:32 -0200

    Fix injections param to be used only in dev profile

--- project.clj
+++ project.clj
@@ -2,20 +2,20 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "All Rights Reserved"}
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+  :repositories {"my.datomic.com" {:url      "https://my.datomic.com/repo"
                                    :username :env/datomic_username
                                    :password :env/datomic_password}}
   :deploy-repositories [["clojars" {:url      "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password}]]
-  :injections [(require 'common-labsoft.misc)
-               (require 'common-labsoft.time)]
   :plugins [[lein-midje "3.2.1"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [labsoft-2018/common-labsoft "0.9.0-SNAPSHOT"]]
   :resource-paths ["resources"]
   :min-lein-version "2.0.0"
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "orders.service/start"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]}
+  :profiles {:dev     {:aliases      {"run-dev" ["trampoline" "run" "-m" "orders.service/start"]}
+                       :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]
+                       :injections   [(require 'common-labsoft.misc)
+                                      (require 'common-labsoft.time)]}
              :uberjar {:aot [orders.service]}}
   :main ^{:skip-aot true} orders.service)
